4a3119a66f5f3d8f048af81b8b87ebed3061422d
Chen Jia <cjia@c4r1s7.42tokyo.jp>
2024-06-14 16:19:07 +0900

    finish

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -1,89 +1,92 @@
 #include "PhoneBook.hpp"
 
-PhoneBook::PhoneBook() : contact_num(0), contact_size(0) {}
+PhoneBook::PhoneBook()
+	: contact_num(0), contact_size(0)
+{
+}
 
-void error_mes_with_exit()
+void	error_mes_with_exit(void)
 {
 	std::cout << "error" << std::endl;
 	std::exit(0);
 }
 
 //印字可能であるかどうかを判定
-bool check_print(std::string str)
+bool	check_print(std::string str)
 {
-	char ch;
-	for(int i = 0; i < str.size(); i++)
+	char	ch;
+
+	for (int i = 0; i < str.size(); i++)
 	{
 		ch = str[i];
-		if(std::isprint(ch) == 0)
-			return(false);
+		if (std::isprint(ch) == 0)
+			return (false);
 	}
-
-	return true;
+	return (true);
 }
 
 //文字列にスペースが含まれているかどうかをチェック
-bool contains_non_space_char(std::string str)
+bool	contains_non_space_char(std::string str)
 {
-	int i = 0;
-	if(str.empty())
+	int	i;
+
+	i = 0;
+	if (str.empty())
 	{
 		std::cout << "Please enter again" << std::endl;
-		return false;
+		return (false);
 	}
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
-		if(std::isspace(str[i]) == 0)
+		if (std::isspace(str[i]) == 0)
 			return (true);
 		i++;
 	}
 	std::cout << "Please enter again" << std::endl;
-		return (false);
+	return (false);
 }
 
 #define SEARCH_PROMPT ">"
-void Contact:: input_info(int contact_index)
+
+void Contact::input_info(int contact_index)
 {
 	std::string input;
-	while(true)
+	while (true)
 	{
 		std::cout << SEARCH_PROMPT;
 		std::getline(std::cin, input);
 		if (std::cin.eof() || std::cin.fail() || check_print(input) == false)
 			error_mes_with_exit();
-		if(contains_non_space_char(input) == false)
-			continue;
-		break;
+		if (contains_non_space_char(input) == false)
+			continue ;
+		break ;
 	}
-	std::string *assigned_member_array[] = \
-	{&m_firstname, &m_lastname, &m_nickname, &m_phonenumber, &m_darkestsecret};
+	std::string *assigned_member_array[] =
+		{&m_firstname, &m_lastname, &m_nickname, &m_phonenumber,
+			&m_darkestsecret};
 	*assigned_member_array[contact_index] = input;
 }
-
 
 void PhoneBook::add()
 {
 	contact_num = contact_num % MAX_NUM;
-
-	std::string assigned_member_array[] = \
-	{   "----enter first name----",
-        "----enter last name----",
-        "----enter nickname----",
-        "----enter phone number----",
-        "----enter darkest secret----"
-	};
-
+	std::string assigned_member_array[] =
+		{"----enter first name----",
+			"----enter last name----",
+			"----enter nickname----",
+			"----enter phone number----",
+			"----enter darkest secret----"};
 	for (int i = 0; i < 5; i++)
 	{
 		std::cout << assigned_member_array[i] << std::endl;
 		contact[contact_num].input_info(i);
 	}
-	if(contact_size < MAX_NUM)
+	if (contact_size < MAX_NUM)
 		contact_size++;
 	contact_num++;
 }
 
-int	PhoneBook::string_to_int(const std::string str)
+int PhoneBook::string_to_int(const std::string str)
 {
 	int	result;
 
@@ -101,7 +104,6 @@ std::string change_length(std::string str)
 	else
 		return (str.substr(0, 9) + ".");
 }
-
 
 void PhoneBook::search()
 {
@@ -121,22 +123,19 @@ void PhoneBook::search()
 		std::cout << std::right << std::setw(10) << change_length(contact[i].get_nickname()) << std::endl;
 	}
 	std::cout << "-------------------------------------------" << std::endl;
-
-	while(true)
+	while (true)
 	{
 		std::cout << "Enter index" << std::endl;
 		std::getline(std::cin, input);
-		if(input[0] >= '0' && input[0] < contact_size + ASCII_NUM && input.substr(1, 1).empty() && check_print(input) == true)
-			break;
+		if (input[0] >= '0' && input[0] < contact_size + ASCII_NUM
+			&& input.substr(1, 1).empty() && check_print(input) == true)
+			break ;
 		else
-			continue;
+			continue ;
 	}
-	int index = string_to_int(input);//ここを考慮する
-	if (index == -1)
-		return ;
-	std::cout << "First name: " << contact[index].get_firstname() << std::endl;
-	std::cout << "Last name: " << contact[index].get_lastname() << std::endl;
-	std::cout << "Nick name: " << contact[index].get_nickname() << std::endl;
-	std::cout << "Phone number: " << contact[index].get_phonenumber() << std::endl;
-	std::cout << "Darkest secret: " << contact[index].get_darkestsecret() << std::endl;
+	std::cout << "First name: " << contact[string_to_int(input)].get_firstname() << std::endl;
+	std::cout << "Last name: " << contact[string_to_int(input)].get_lastname() << std::endl;
+	std::cout << "Nick name: " << contact[string_to_int(input)].get_nickname() << std::endl;
+	std::cout << "Phone number: " << contact[string_to_int(input)].get_phonenumber() << std::endl;
+	std::cout << "Darkest secret: " << contact[string_to_int(input)].get_darkestsecret() << std::endl;
 }
